9e833cd550ddc81d62f0ce6b7d54e4bca846e3cc
Gr1Zi <117056178+Gr1Zi@users.noreply.github.com>
2022-10-31 13:53:40 +0800

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,8 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         Fraction fraction1 = new Fraction();
-        Fraction fraction2 = new Fraction(4,7);
-        Fraction fraction3 = new Fraction(3,4);
+        Fraction fraction2 = new Fraction(6,10);
+        Fraction fraction3 = new Fraction(2,4);
 
         fraction1 = fraction2.shareFraction(fraction3);
         System.out.println(fraction1 + " Деление");
